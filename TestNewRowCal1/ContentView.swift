4c96e8a6c49ca54645a57ff2ebe033a206d74a1f
zyaaa <zybrave@gmail.com>
2024-09-06 12:41:08 +0800

    Initial Commit

---
+++ TestNewRowCal1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestNewRowCal1
+//
+//  Created by Ethan on 2024-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestNewRowCal1/TestNewRowCal1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewRowCal1App.swift
+//  TestNewRowCal1
+//
+//  Created by Ethan on 2024-09-06.
+//
+
+import SwiftUI
+
+@main
+struct TestNewRowCal1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
